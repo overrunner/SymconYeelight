366f8a46ec5093a032db6dbf7ab0832b3694eaf7
Steffen Bornemann <steffen@fitrock.com>
2017-12-27 13:12:39 +0100

    Initial Import

--- YeelightColorBulb/module.php
+++ YeelightColorBulb/module.php
@@ -3,7 +3,6 @@
 class YeelightColorBulb extends IPSModule {
 
     // Der Konstruktor des Moduls
-    // Überschreibt den Standard Kontruktor von IPS
     public function __construct($InstanceID) {
         // Diese Zeile nicht löschen
         parent::__construct($InstanceID);
@@ -11,7 +10,6 @@ class YeelightColorBulb extends IPSModule {
         // Selbsterstellter Code
     }
 
-    // Überschreibt die interne IPS_Create($id) Funktion
     public function Create() {
         parent::Create();
 
@@ -26,16 +24,18 @@ class YeelightColorBulb extends IPSModule {
 
     }
 
-    // Überschreibt die intere IPS_ApplyChanges($id) Funktion
+
     public function ApplyChanges() {
         // Diese Zeile nicht löschen
         parent::ApplyChanges();
 
         $this->RegisterVariableString("name", "Name", "~String",1);
         $this->RegisterVariableBoolean("power", "Power", "~Switch",2);
-        $this->RegisterVariableInteger("dim", "Dimmer", "~Intensity.100",3);
+        $this->RegisterVariableInteger("brightness", "Brightness", "~Intensity.100",3);
 
         $this->GetConfigurationForParent();
+
+
     }
 
     public function GetConfigurationForParent()
@@ -49,41 +49,22 @@ class YeelightColorBulb extends IPSModule {
 
     // Lese alle Konfigurationsdaten aus
     public function readStatesFromDevice() {
-        $ip = $this->ReadPropertyString("ipadress");
-        $url = "http://".$ip.":8080/api/v2/device";
-        $response = LM_callapi($this->InstanceID, $url, array(), "GET");
-        $data = json_decode($response);
-        if ($data->display->brightness_mode == "auto") { $mode=true; } else { $mode=false; };
-        SetValue(IPS_GetObjectIDByName("Volume", $this->InstanceID), $data->audio->volume);
-        SetValue(IPS_GetObjectIDByName("Helligkeit", $this->InstanceID),$data->display->brightness);
-        SetValueBoolean(IPS_GetObjectIDByName("Helligkeit Auto Modus", $this->InstanceID),$mode);
-        SetValueBoolean(IPS_GetObjectIDByName("Bluetooth", $this->InstanceID),$data->bluetooth->active);
-        SetValue(IPS_GetObjectIDByName("Bluetooth Name", $this->InstanceID),$data->bluetooth->name);
-        SetValue(IPS_GetObjectIDByName("Name", $this->InstanceID),$data->name);
-        SetValue(IPS_GetObjectIDByName("OS Version", $this->InstanceID),$data->os_version);
-        SetValue(IPS_GetObjectIDByName("SSID", $this->InstanceID),$data->wifi->essid);
-        SetValue(IPS_GetObjectIDByName("WLan Empfang", $this->InstanceID),$data->wifi->strength);
-        if (file_exists(IPS_GetKernelDir()."/scripts/LM_setdisplay.php") == false) {
-            copy(IPS_GetKernelDir()."/modules/Symcon-LaMetric/LaMetric/setdisplay.php", IPS_GetKernelDir()."/scripts/LM_setdisplay.php");
-            copy(IPS_GetKernelDir()."/modules/Symcon-LaMetric/LaMetric/setbluetooth.php", IPS_GetKernelDir()."/scripts/LM_setbluetooth.php");
-            copy(IPS_GetKernelDir()."/modules/Symcon-LaMetric/LaMetric/setvolume.php", IPS_GetKernelDir()."/scripts/LM_setvolume.php");
-        }
-        return $data;
+
+        $commandStr = $this->buildCommandString('get_prop', array('name', 'power', 'bright'));
+        $result = $this->SendDataToParent($commandStr."\r\n");
+        SetValue(IPS_GetObjectIDByName("name", $this->InstanceID), $result);
     }
 
 
 
 
-    private function SendToYeelight($method, $params) {
+    private function buildCommandString($method, $params) {
         $Data = Array(
             'id' => 1,
             'method' => $method,
             'params' => $params
         );
-        $Line = json_encode($Data);
-
-        $ip = $this->ReadPropertyString("ipadress");
-
+        return json_encode($Data);
     }
 }
 ?>
